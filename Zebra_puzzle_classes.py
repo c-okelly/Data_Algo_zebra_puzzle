1084bd855466237c7ccf708e96b579e20e1a9c62
cokelly <okellyconor@gmail.com>
2016-04-08 14:14:00 +0100

    Set up to main files for code. Start rough outline in classes file

---
+++ Zebra_puzzle_classes.py
@@ -0,0 +1,14 @@
+# Author Conor O'Kelly
+# This file will contain all of the classes required by the puzzle
+
+
+class Problem:
+
+    def __repr__(self):
+        return "The problem class"
+
+
+class Constrain:
+
+    def __repr__(self):
+        return "Constraints for the current puzzle"
---
+++ Zebra_puzzle_main.py
@@ -0,0 +1,12 @@
+# Author = Conor O'Kelly
+# This file will contain all of the code required to solve the zebra puzzle
+
+
+def main():
+    return
+
+
+
+
+if __name__ == '__main__':
+    main()
